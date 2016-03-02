a20cfd9967c18c7ebd45a99ac641b8a2dd98a7d9
Per Nilsson <per.nilsson@formidablelabs.com>
2016-03-02 10:26:31 -0800

    Replace demo app with a repro for #133

--- demo/app.jsx
+++ demo/app.jsx
@@ -1,45 +1,60 @@
 /*global document:false */
 import React from "react";
 import ReactDOM from "react-dom";
-import AxisDemo from "./components/victory-axis-demo";
-import BarDemo from "./components/victory-bar-demo";
-import ChartDemo from "./components/victory-chart-demo";
-import LineDemo from "./components/victory-line-demo";
-import ScatterDemo from "./components/victory-scatter-demo";
-import { Router, Route, Link } from "react-router";
+import _ from "lodash";
+
+import {VictoryBar} from "../src/";
 
 const content = document.getElementById("content");
 
-const App = React.createClass({
-  propTypes: {
-    children: React.PropTypes.element
-  },
+class App extends React.Component {
+   constructor(props) {
+    super(props);
+    this.state = {
+      data: this.getData(),
+    };
+  }
+
+  getData() {
+    return _.map(_.range(5), (index) => {
+      return [
+        {x: "apples", y: _.random(1, 5)},
+        {x: "oranges", y: _.random(1, 5)},
+        {x: "bananas", y: _.random(1, 5)}
+      ];
+    });
+  }
+
+  componentDidMount() {
+    setInterval(() => {
+      this.setState({
+        data: this.getData(),
+      });
+    }, 3000);
+  }
 
   render() {
     return (
-      <div>
-        <h1>Victory Chart and Friends Demo</h1>
-        <ul>
-          <li><Link to="/chart">Victory Chart Demo</Link></li>
-          <li><Link to="/axis">Victory Axis Demo</Link></li>
-          <li><Link to="/bar">Victory Bar Demo</Link></li>
-          <li><Link to="/line">Victory Line Demo</Link></li>
-          <li><Link to="/scatter">Victory Scatter Demo</Link></li>
-        </ul>
-        {this.props.children}
-      </div>
+      <VictoryBar
+        height={600}
+        padding={75}
+        domain={{
+          x: [1, 3],
+          y: [0, 5]
+        }}
+        animate={{velocity: 0.02}}
+        data={this.state.data}
+        dataAttributes={[
+          {fill: "cornflowerblue"},
+          {fill: "tomato"},
+          {fill: "orange"},
+          {fill: "gold"}
+        ]}
+      />
     );
   }
-});
+}
 
 ReactDOM.render((
-  <Router>
-    <Route path="/" component={App}>
-      <Route path="axis" component={AxisDemo}/>
-      <Route path="bar" component={BarDemo}/>
-      <Route path="chart" component={ChartDemo}/>
-      <Route path="line" component={LineDemo}/>
-      <Route path="scatter" component={ScatterDemo}/>
-    </Route>
-  </Router>
+  <App/>
 ), content);
